9367e2c72d33d9bda7600156f89cb02f17c093f3
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-25 11:07:24 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Helloworld';
+
+  imgUrl = "../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = ""; 
+
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+  }
+
+
+  onClick($event: Event): void {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+}
+
