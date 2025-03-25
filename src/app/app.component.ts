b7f03fe41b290dd7acf089c1fc594a0506da27c1
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-25 11:12:18 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Helloworld';
+
+  imgUrl="../assets/BL_logo_square_jpg.jpg";
+
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+  }
+}
